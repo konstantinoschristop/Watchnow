b6c7aef9d24ff72ce2a48dbb71ae544a70f0ac9b
Konstantinos Christopoulos <k.christopoulos@accenture.com>
2021-11-27 18:53:15 +0200

    added refreshable

--- Watchnow/View/MoviesView.swift
+++ Watchnow/View/MoviesView.swift
@@ -14,10 +14,13 @@ struct MoviesView: View {
     
     var body: some View {
 
-        VStack(spacing: -10) {
+        List {
             
             if let results = upcomingMoviesVM.upcomingMovies.results {
                 TopView(results: results, viewTitle: "Upcoming Movies")
+                    .listRowSeparatorTint(.clear)
+                    .listRowBackground(Color.clear)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             } else {
                 ProgressView()
             }
@@ -26,11 +29,19 @@ struct MoviesView: View {
             
             if let results = popularMoviesVM.popularMovies.results {
                 BottomView(results: results, viewTitle: "Popular Movies")
+                    .listRowSeparatorTint(.clear)
+                    .listRowBackground(Color.clear)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             } else {
                 ProgressView()
             }
         }
+        .listStyle(.plain)
         .task {
+            await upcomingMoviesVM.getUpcomingMovies()
+            await popularMoviesVM.getPopularMovies()
+        }
+        .refreshable {
             await upcomingMoviesVM.getUpcomingMovies()
             await popularMoviesVM.getPopularMovies()
         }
